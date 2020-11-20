a6ec68b2de1a291297ba58f32563fa381df289b8
Nightpie <71722996+Nightpie@users.noreply.github.com>
2020-11-20 13:13:20 -0600

    Update chrisDBMS.sql

--- chrisDBMS.sql
+++ chrisDBMS.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `placed_order`
 (
-`Id` int,
+`id` int,
 `Restarant_id` int,
 `Order_time` timestamp,
 `Estimated_delivery_time` timestamp,
@@ -12,23 +12,26 @@ CREATE TABLE `placed_order`
 `discount` decimal(12,2),
 `final price` decimal(12,2),
 `comment` text,
-`ts` timestamp
+`ts` timestamp,
+PRIMARY KEY (id),
+FOREIGN KEY (`Customer id`) REFERENCES `customer:orders`(id)
 );
-
 CREATE TABLE `comment`
 (
-`Id` int,
+`id` int,
 `Placed_order_id` int,
 `Customer_id` int,
 `Comment_text` text,
 `ts` timestamp,
 `Is_complaint` bool,
-`Is_praise` bool
+`Is_praise` bool,
+PRIMARY KEY (id),
+FOREIGN KEY (`Customer id`) REFERENCES `customer:orders`(id),
+FOREIGN KEY (`Placed_order_id`) REFERENCES Placed_order(id)
 );
-
 CREATE TABLE `customer:orders`
 (
-`Id` int,
+`id` int,
 `customer_name` varchar(30),
 `city_id` int,
 `address` varchar(50),
@@ -36,31 +39,38 @@ CREATE TABLE `customer:orders`
 `email` varchar(35),
 `confirmation_code` varchar(30),
 `password` varchar(30),
-`time_joined` timestamp
+`time_joined` timestamp,
+PRIMARY KEY (id),
+FOREIGN KEY (city_id) REFERENCES city(id)
 );
-
 CREATE TABLE `in_order`
 (
-`Id` int,
+`id` int,
 `placed_order_id` int,
-`offer-id` int,
+`offer_id` int,
 `menu_item_id` int,
 `quantity` int,
 `item_price` decimal(12,2),
 `price` decimal(12,2),
-`comment` varchar(50)
+`comment` varchar(50),
+PRIMARY KEY (id),
+FOREIGN KEY (placed_order_id) REFERENCES placed_order(id),
+FOREIGN KEY (offer_id) REFERENCES offer(id),
+FOREIGN KEY (menu_item_id) REFERENCES menu_item(id)
 );
-
 CREATE TABLE `order_status`
 (
-`Id` int,
+`id` int,
 `placed_order_id` int,
 `status_catalog_id` int,
-`ts` timestamp
+`ts` timestamp,
+PRIMARY KEY (id),
+FOREIGN KEY (placed_order_id) REFERENCES placed_order(id),
+FOREIGN KEY (status_catalog_id) REFERENCES status_catalog(id)
 );
-
 CREATE TABLE `status_catalog`
 (
-`Id` int,
-`status_name` varchar(20)
+`id` int,
+`status_name` varchar(20),
+PRIMARY KEY (id)
 );
