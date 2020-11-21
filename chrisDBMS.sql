6130248fbd2a4f7a709ab948fa114f18e69e1601
Nightpie <71722996+Nightpie@users.noreply.github.com>
2020-11-21 14:59:10 -0600

    Update chrisDBMS.sql

--- chrisDBMS.sql
+++ chrisDBMS.sql
@@ -1,58 +1,51 @@
 CREATE TABLE `placed_order`
 (
-`id` int,
-`Restarant_id` int,
-`Order_time` timestamp,
-`Estimated_delivery_time` timestamp,
-`Food ready` timestamp,
-`Actual_delivery_time` timestamp,
-`Delivery_address` varchar(50),
-`Customer id` int,
-`price` decimal(12,2),
-`discount` decimal(12,2),
-`final price` decimal(12,2),
-`comment` text,
-`ts` timestamp,
+`id` int NOT NULL,
+`Restarant_id` int NOT NULL,
+`Delivery_address` varchar(50) NOT NULL,
+`Customer id` int NOT NULL,
+`price` decimal(12,2) NOT NULL CHECK (price >= 0),
+`discount` decimal(12,2) NOT NULL CHECK (discount >= 0),
+`final price` decimal(12,2) NOT NULL CHECK (`final price` >= 0),
+`comment` text NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (`Customer id`) REFERENCES `customer:orders`(id)
 );
 CREATE TABLE `comment`
 (
-`id` int,
-`Placed_order_id` int,
-`Customer_id` int,
-`Comment_text` text,
-`ts` timestamp,
-`Is_complaint` bool,
-`Is_praise` bool,
+`id` int NOT NULL,
+`Placed_order_id` int NOT NULL,
+`Customer_id` int NOT NULL,
+`Comment_text` text NOT NULL,
+`Is_complaint` bool NULL,
+`Is_praise` bool NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (`Customer id`) REFERENCES `customer:orders`(id),
 FOREIGN KEY (`Placed_order_id`) REFERENCES Placed_order(id)
 );
 CREATE TABLE `customer:orders`
 (
-`id` int,
-`customer_name` varchar(30),
-`city_id` int,
-`address` varchar(50),
-`contact_phone` varchar(25),
-`email` varchar(35),
-`confirmation_code` varchar(30),
-`password` varchar(30),
-`time_joined` timestamp,
+`id` int NOT NULL,
+`customer_name` varchar(30) NOT NULL,
+`city_id` int NOT NULL,
+`address` varchar(50) NOT NULL,
+`contact_phone` varchar(25) NOT NULL,
+`email` varchar(35) NOT NULL,
+`confirmation_code` varchar(30) NOT NULL,
+`password` varchar(30) NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (city_id) REFERENCES city(id)
 );
 CREATE TABLE `in_order`
 (
-`id` int,
-`placed_order_id` int,
-`offer_id` int,
-`menu_item_id` int,
-`quantity` int,
-`item_price` decimal(12,2),
-`price` decimal(12,2),
-`comment` varchar(50),
+`id` int NOT NULL,
+`placed_order_id` int NOT NULL,
+`offer_id` int NOT NULL,
+`menu_item_id` int NOT NULL,
+`quantity` int NOT NULL CHECK (quantity >= 0),
+`item_price` decimal(12,2) NOT NULL CHECK (item_price >= 0),
+`price` decimal(12,2) NOT NULL CHECK (price >= 0),
+`comment` varchar(50) NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (placed_order_id) REFERENCES placed_order(id),
 FOREIGN KEY (offer_id) REFERENCES offer(id),
@@ -60,17 +53,16 @@ FOREIGN KEY (menu_item_id) REFERENCES menu_item(id)
 );
 CREATE TABLE `order_status`
 (
-`id` int,
-`placed_order_id` int,
-`status_catalog_id` int,
-`ts` timestamp,
+`id` int NOT NULL,
+`placed_order_id` int NOT NULL,
+`status_catalog_id` int NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (placed_order_id) REFERENCES placed_order(id),
 FOREIGN KEY (status_catalog_id) REFERENCES status_catalog(id)
 );
 CREATE TABLE `status_catalog`
 (
-`id` int,
-`status_name` varchar(20),
+`id` int NOT NULL,
+`status_name` varchar(20) NOT NULL CHECK (status_name IN('Preparing food', 'Food is ready', 'Food is on the way')),
 PRIMARY KEY (id)
 );
