69f57dd2b42775eb2f8c02e078b0d12dfa2ab3eb
Saivion Hayes <52749505+Saivion@users.noreply.github.com>
2020-11-22 01:53:57 -0600

    Update and rename On-Demand.sql to dbms-project

--- On-Demand.sql
+++
@@ -1,7 +0,0 @@
--- CREATE TABLE Guests(guestNo varchar(3), fName varchar (20), lName varchar(20), foodType varchar(20));
--- INSERT INTO Guests VALUES('1', 'Saivion', 'Hayes', 'Soul Food');
--- SELECT * FROM Guests;
-
--- create table Branch (branchNo varchar(4), street varchar(20), city varchar(20), postcode varchar(7));
--- describe Branch;
-insert into Branch (
