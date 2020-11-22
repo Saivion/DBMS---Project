f5befb613e7eb8edd2d217f20c66a8e4bef643b1
Victor Akpan <viic@Victors-MacBook-Pro.local>
2020-11-21 22:04:57 -0600

    fixed format issue

--- victorDBMS.sql
+++ victorDBMS.sql
@@ -1,16 +1,12 @@
 CREATE TABLE City ( cityID int NOT NULL, 
-									 city  varchar(255) NOT NULL CHECK (city not like '%[^0-9]%') , 
-									 state  varchar(255) NOT NULL CHECK (state not like '%[^0-9]%'),
-									 zipCode varchar(255) NOT NULL, 
-									 PRIMARY KEY(cityID));
+city  varchar(255) NOT NULL CHECK (city not like '%[^0-9]%') , 
+state  varchar(255) NOT NULL CHECK (state not like '%[^0-9]%'),
+zipCode varchar(255) NOT NULL, 
+PRIMARY KEY(cityID));
                                                 
 CREATE TABLE Restaurant ( restaurantID int NOT NULL, 
-												restaurantName varchar(255) NOT NULL, 
-												address  varchar(255) NOT NULL , 
-                                                cityID int NOT NULL, 
-                                                PRIMARY KEY(restaurantID),
-                                                FOREIGN KEY (cityID) references City(cityID));
-                                                
-
-
-drop table Restaurant
+restaurantName varchar(255) NOT NULL, 
+address  varchar(255) NOT NULL , 
+cityID int NOT NULL, 
+PRIMARY KEY(restaurantID),
+FOREIGN KEY (cityID) references City(cityID));
