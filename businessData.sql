679ced37254655b84d393b280c58151127ac778b
Victor Akpan <viic@Victors-MacBook-Pro.local>
2020-11-24 00:31:41 -0600

    added data

--- businessData.sql
+++ businessData.sql
@@ -1,1 +1,36 @@
+INSERT INTO city (city, state)
+VALUES ('Richmond', 'TX');
+INSERT INTO city (city, state)
+VALUES ('Prairie View', 'TX');
+INSERT INTO city (city, state)
+VALUES ('Houston', 'TX');
+INSERT INTO city (city, state)
+VALUES ('Katy', 'TX');
 
+INSERT INTO restaurant (restaurant_name, street_address, zip_code, city_id)
+VALUES ('Chick-fil-A', '5644 W Grand Pkwy S', 77406, 2);
+
+INSERT INTO customer (customer_name, street_address, zip_code, city_id, email, phone, date_joined)
+VALUES ('John AppleSeed', '6207 Katy-Gaston Rd', 77494, 3, 'jseed@email.com', '111-111-1111', '2020-01-14');
+
+INSERT INTO food_category (category_name)
+VALUES ('American');
+INSERT INTO food_category (category_name)
+VALUES ('Healthy');
+
+INSERT INTO menu_item (item_name, price, rating, category_id)
+VALUES ('Cobb Salad', 7.99, 4, 2);
+INSERT INTO menu_item (item_name, price, rating, category_id)
+VALUES ('Mac & Cheese', 3.99, 3, 1);
+
+INSERT INTO placed_order (restaurant_id, customer_id, delivery_address, price, final_price, date_ordered)
+VALUES (1, 2, '25330 Kingsland Blvd',11.98 , 11.98, '2020-02-11');
+
+INSERT INTO in_order (placed_order_id, menu_item_id, quantity, item_price, total_price)
+VALUES (1, 2, 1,3.99 , 3.99);
+INSERT INTO in_order (placed_order_id, menu_item_id, quantity, item_price, total_price)
+VALUES (1, 1, 1,7.99 , 7.99);
+
+
+INSERT INTO order_status (placed_order_id, order_status)
+VALUES (1, 'Delivered');
--- officialDB.sql
+++ officialDB.sql
@@ -1,71 +1,72 @@
 CREATE TABLE city( 
-city  varchar(20) NOT NULL CHECK (city not like '%[^0-9]%'), 
-state  varchar(15) NOT NULL CHECK (state not like '%[^0-9]%'),
-zip_code int NOT NULL, 
-PRIMARY KEY(zip_code));
+city_id int NOT NULL AUTO_INCREMENT,
+city  varchar(50) NOT NULL CHECK (city not like '%[^0-9]%'), 
+state  varchar(15) NOT NULL CHECK (state not like '%[^0-9]%'), 
+PRIMARY KEY(city_id));
 
 CREATE TABLE restaurant( 
-restaurant_id int NOT NULL, 
-restaurant_name varchar(25) NOT NULL, 
-address  varchar(50) NOT NULL , 
-zip_code int, 
+restaurant_id int NOT NULL AUTO_INCREMENT, 
+restaurant_name varchar(50) NOT NULL, 
+street_address  varchar(50) NOT NULL, 
+zip_code int NOT NULL, 
+city_id int,
 PRIMARY KEY(restaurant_id),
-FOREIGN KEY (zip_code) references city(zip_code)); 
+FOREIGN KEY (city_id) references city(city_id)); 
 
 CREATE TABLE customer(
-customer_id int NOT NULL,
+customer_id int NOT NULL AUTO_INCREMENT,
 customer_name varchar(255) NOT NULL,
-zip_code int,
-address varchar(50) NOT NULL,
-contact_phone varchar(25) NOT NULL,
+street_address varchar(50) NOT NULL,
+zip_code int NOT NULL,
+city_id int,
 email varchar(30) NOT NULL,
-date_joined date,
-password varchar(20) NOT NULL,
+phone varchar(25) NOT NULL,
+date_joined date NOT NULL,
 PRIMARY KEY (customer_id),
-FOREIGN KEY (zip_code) REFERENCES city(zip_code));
+FOREIGN KEY (city_id) REFERENCES city(city_id));
 
 create table food_category(
-category_id int NOT NULL,
-category_name varchar(50),
+category_id int NOT NULL AUTO_INCREMENT,
+category_name varchar(50) NOT NULL,
 PRIMARY KEY (category_id));
 
 create table menu_item(
-item_id int NOT NULL, 
+item_id int NOT NULL AUTO_INCREMENT, 
 item_name varchar(50) NOT NULL,
-category_id int NOT NULL,
-description text,
+description text NULL,
 price decimal(3,2) NOT NULL,
-rating int,
+rating int NULL,
+category_id int,
 PRIMARY KEY (item_id),
 FOREIGN KEY (category_id) references food_category(category_id));
 
 CREATE TABLE placed_order(
-order_id int NOT NULL,
-restarant_id int NOT NULL,
+order_id int NOT NULL AUTO_INCREMENT,
+restaurant_id int,
+customer_id int,
 delivery_address varchar(50) NOT NULL,
-customer_id int NOT NULL,
-price decimal(3,2) NOT NULL CHECK (price >= 0),
-discount decimal(3,2) CHECK (discount >= 0),
-final_price decimal(3,2) NOT NULL CHECK (final_price >= 0),
-comment text NULL,
+price decimal(5,2) NOT NULL CHECK (price >= 0),
+discount decimal(5,2) NULL,
+final_price decimal(5,2) NOT NULL CHECK (final_price >= 0),
 date_ordered date,
-comment_text text NOT NULL,
+comment text  NULL,
 PRIMARY KEY (order_id),
-FOREIGN KEY (customer_id) REFERENCES customer(customer_id));
+FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+FOREIGN KEY (restaurant_id) REFERENCES restaurant(restaurant_id));
 
 CREATE TABLE in_order(
 placed_order_id int,
-menu_item_id int NOT NULL,
+menu_item_id int,
 quantity int NOT NULL CHECK (quantity >= 0),
 item_price decimal(3,2) NOT NULL CHECK (item_price >= 0),
-price decimal(3,2) NOT NULL CHECK (price >= 0),
-comment varchar(50) NULL,
+total_price decimal(5,2) NOT NULL CHECK (total_price >= 0),
+comment text  NULL,
 FOREIGN KEY (placed_order_id) REFERENCES placed_order(order_id),
 FOREIGN KEY (menu_item_id) REFERENCES menu_item(item_id));
 
 CREATE TABLE order_status(
-status_id int NOT NULL,
-placed_order_id int NOT NULL,
-status_name varchar(20) NOT NULL CHECK (status_name IN('Preparing food', 'Food is ready', 'Food is on the way')),
+status_id int NOT NULL AUTO_INCREMENT,
+placed_order_id int,
+order_status varchar(20) NOT NULL CHECK (order_status IN('Preparing food', 'Food is ready', 'Food is on the way', 'Delivered')),
 PRIMARY KEY (status_id),
 FOREIGN KEY (placed_order_id) REFERENCES placed_order(order_id));
